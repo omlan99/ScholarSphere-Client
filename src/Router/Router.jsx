3d363d4bc186dde2caea4a172627cd36e52a6377
omlan99 <omlan10616@gmail.com>
2025-01-20 16:57:13 +0600

    error page added

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -6,12 +6,13 @@ import MainLayout from "../Layout/MainLayout";
 import Home from "../Pages/Home/Home";
 import Login from "../Pages/Login/Login";
 import SignUp from "../Pages/Login/SignUp";
+import ErrorPage from "../Pages/Error/ErrorPage";
 
 export const router = createBrowserRouter([
     {
       path: "/",
       element: <MainLayout></MainLayout>,
-      
+      errorElement : <ErrorPage></ErrorPage>,
       children : [
         {
             path: "/signup",

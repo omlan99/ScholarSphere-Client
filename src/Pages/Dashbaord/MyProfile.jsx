129fd44188bd534a0177ba6363f5e77f2945fba8
omlan99 <omlan10616@gmail.com>
2025-01-29 07:25:15 +0600

    profile data visible

--- src/Pages/Dashbaord/MyProfile.jsx
+++ src/Pages/Dashbaord/MyProfile.jsx
@@ -2,19 +2,21 @@ import React, { useEffect, useState } from "react";
 import useCommonAxios from "../../Hook/useCommonAxios";
 import useAuth from "../../Hook/useAuth";
 import SectionTitle from "../../Component/SectionTitle";
+import useAxiosSecure from "../../Hook/useAxiosSecure";
 
 const MyProfile = () => {
   const { user } = useAuth();
-  const [profile, setProfile] = useState({});
-
-  const email = user?.email;
+  const [profile, setProfile] = useState([]);
+  const axiosSecure = useAxiosSecure()
+  
   const axiosCommon = useCommonAxios();
   useEffect(() => {
-    axiosCommon.get(`/users?email=${email}`).then((res) => {
-      // console.log(res.data);
+    axiosSecure.get(`/users/${user?.email}`).then((res) => {
+      console.log(res.data);
       setProfile(res.data);
     });
-  }, [email]);
+  }, [user?.email, axiosSecure]);
+  console.log(profile)
   return (
     <div>
       <SectionTitle heading={"My Profile"}></SectionTitle>
@@ -25,7 +27,7 @@ const MyProfile = () => {
         <div>
           <p>Name : {profile.name} </p>
           <p> Email : {profile.email}</p>
-          {profile.role ? <p>{profile.role}</p> : <></>}
+          <p> Role : {profile.role ? <span>{profile.role}</span> : <></>}</p>
         </div>
       </div>
     </div>
